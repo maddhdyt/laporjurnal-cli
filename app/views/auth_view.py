fb7ad8a8899eaaaba93f3e9b65e8adc33ab66b51
maddhdyt <madd.hdyt@gmail.com>
2024-12-29 13:38:36 +0700

    wipe: ver/excel_ver to be replaced

--- app/views/auth_view.py
+++
@@ -1,8 +0,0 @@
-def show_register_user():
-    print("\n=== Register User ===")
-
-def show_register_validator():
-    print("\n=== Register Validator ===")
-
-def show_login():
-    print("\n=== Login ===")
